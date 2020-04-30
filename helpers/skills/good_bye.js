e9ce305619f21a8afccdc64906eaaa71ad6ce841
Nishant Srivastava <nisrulz@gmail.com>
2020-05-01 00:34:43 +0200

    updated good bye skill

--- helpers/skills/good_bye.js
+++ helpers/skills/good_bye.js
@@ -17,12 +17,14 @@ function listenOnWords() {
         'See everyone tomorrow', 'see you all tomorrow', 'Schönenfeierabend', 'Have a nice evening everyone',
         'Schoenenfeierabend', 'Have a nice weekend everyone!', 'See you later', 'Signing off for the day', 'I can leave',
         'leaving now', 'G0od bY3', 'Have a good evening', 'Schoenenfeireabend', 'C you all', 'Cya', 'See you all',
-        'বিদায়', 'ciao', 'adios', 'अलविदा', 'Hoşçakal'
+        'বিদায়', 'ciao', 'adios', 'अलविदा', 'Hoşçakal', 'Schönes Wochenende', 'Logging off', 'Have a nice extended weekend',
+        'have a nice weekend', 'I am off', 'Done for the week', 'Done for the day', 'Done for today', ':wave:', 'Catch you all tomorrow',
+        'Schönen Feierabend', 'Bis morgen'
     ]
 }
 
 function getEmoji() {
-    const emojiList = ['wave']
+    const emojiList = ['wave', 'runner', 'sonic', 'nyancat_big', 'fidget_spinner']
     const randomEmoji = utils.getRandomItemFromArray(emojiList)
     return [randomEmoji]
 }
